68119f1ec6c0c3e62a8586e013a8f15657e28800
k.klimas <kanyqan@gmail.com>
2024-12-30 19:18:20 +0100

    tailwindCSS exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'wczesnoszkolni';
+
+  @HostListener('document:click', ['$event']) onDocumentClick(
+    event: MouseEvent
+  ) {
+    const clickedInside = (event.target as HTMLElement).closest(
+      '#avatarButton'
+    );
+    if (!clickedInside) {
+      this.toggleMenu();
+    }
+  }
+
+  toggleMenu() {
+    var menu = document.getElementById('menu');
+    menu?.classList.toggle('hidden');
+  }
 }
